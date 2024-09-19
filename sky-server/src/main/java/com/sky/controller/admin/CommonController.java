7d88353bc520dff99ad11e4f63fd6414fc563935
lintreetree <lylztf520@gmail.com>
2024-09-19 18:02:38 +0100

    新增菜品

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -1,5 +1,6 @@
 package com.sky.controller.admin;
 
+import com.sky.constant.MessageConstant;
 import com.sky.result.Result;
 import com.sky.utils.AliOssUtil;
 import io.swagger.annotations.Api;
@@ -52,6 +53,6 @@ public class CommonController {
             log.error("文件上传失败：{}", e);
         }
 
-        return null;
+        return Result.error(MessageConstant.UPLOAD_FAILED);
     }
 }
