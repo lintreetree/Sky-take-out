e00babe594dbea4917912af8b330aea2ca40c8f4
lintreetree <lylztf520@gmail.com>
2024-09-26 17:20:53 +0100

    更换位置

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -1,4 +1,4 @@
-package com.sky.service;
+package com.sky.service.impl;
 
 
 import com.sky.dto.DishDTO;
@@ -6,6 +6,7 @@ import com.sky.entity.Dish;
 import com.sky.entity.DishFlavor;
 import com.sky.mapper.DishFlavorMapper;
 import com.sky.mapper.DishMapper;
+import com.sky.service.DishService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
